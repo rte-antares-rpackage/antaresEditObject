a34d25e20b5559e3aaf70fd767270c53551acb15
pvictor <perrier.victor@gmail.com>
2019-05-02 10:58:34 +0200

    export check antares version

--- R/checkVersion.R
+++ R/checkVersion.R
@@ -1,5 +1,23 @@
 
-is_antares_v7 <- function(opts) {
+#' Is study an Antares v7 study ?
+#'
+#' @param opts
+#'   List of simulation parameters returned by the function
+#'   \code{antaresRead::setSimulationPath} 
+#'
+#' @return a logical, \code{TRUE} if study is v7 or above, \code{FALSE} otherwise
+#' @export
+#'
+#' @examples
+#' 
+#' \dontrun{
+#' # setSimulationPath
+#' 
+#' is_antares_v7()
+#' 
+#' }
+#' 
+is_antares_v7 <- function(opts = antaresRead::simOptions()) {
   new_version <- getOption("antares.version.new", default = 700)
   new_version <- as.numeric(new_version)
   isTRUE(opts$antaresVersion >= new_version)
