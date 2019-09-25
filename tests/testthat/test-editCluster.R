f43046b3a53de33588e409a6d100bbf65db2aac2
Etienne <etienne.sanchez@dreamrs.fr>
2019-09-25 14:17:53 +0200

    Fixed error in tests

--- tests/testthat/test-editCluster.R
+++ tests/testthat/test-editCluster.R
@@ -27,7 +27,7 @@ test_that("Edit nominal capacity", {
 test_that("Edit time series", {
   editCluster(area = "a", cluster_name = "peak",
               time_series = rep(8, 8760), add_prefix = FALSE)
-  res <- fread(file.path(opts$inputPath, "thermal", "series", area, cluster_name, "series.txt"))
+  res <- fread(file.path(opts$inputPath, "thermal", "series", "a", "peak", "series.txt"))
   expect_equal(res$V1, rep(8, 8760))
 })
 
@@ -35,7 +35,7 @@ test_that("Edit pre-process data", {
   m <- matrix(5, nrow = 365, ncol = 6)
   editCluster(area = "a", cluster_name = "peak",
               prepro_data = m, add_prefix = FALSE)
-  res <- fread(file.path(opts$inputPath, "thermal", "prepro", area, cluster_name, "data.txt"))
+  res <- fread(file.path(opts$inputPath, "thermal", "prepro", "a", "peak", "data.txt"))
   m_res <- as.matrix(res)
   dimnames(m_res) <- NULL
   expect_equal(m_res, m)
@@ -45,7 +45,7 @@ test_that("Edit pre-process modulation", {
   m <- matrix(1, nrow = 8760, ncol = 4)
   editCluster(area = "a", cluster_name = "peak",
               prepro_modulation = m, add_prefix = FALSE)
-  res <- fread(file.path(opts$inputPath, "thermal", "prepro", area, cluster_name, "modulation.txt"))
+  res <- fread(file.path(opts$inputPath, "thermal", "prepro", "a", "peak", "modulation.txt"))
   m_res <- as.matrix(res)
   dimnames(m_res) <- NULL
   expect_equal(m_res, m)
