e4f9db68800726296b2a7c8001297c8fefe1e7aa
Etienne <etienne.sanchez@dreamrs.fr>
2019-09-26 18:03:11 +0200

    More tests for writeSeriesPrepro()

--- tests/testthat/test-writeSeriesPrepro.R
+++ tests/testthat/test-writeSeriesPrepro.R
@@ -24,10 +24,14 @@ test_that("Write new prepro data", {
   
   M <- matrix(rep(0.3, 12*6), nrow = 12)
   V1 <- rep(2, 8760)
+  dp <- matrix(rep(2, 24*12), nrow = 24)
+  cv <- matrix(1:8, nrow = 2)
   
   writeSeriesPrepro(area = area, type = "wind",
                     coefficients = M,
-                    translation =  V1)
+                    daily_profile = dp,
+                    translation =  V1,
+                    conversion = cv)
   
   coefficients_file <- file.path(path, "test_case", "input", "wind", "prepro",
                                  area, "data.txt")
@@ -35,9 +39,22 @@ test_that("Write new prepro data", {
   translation_file <- file.path(path, "test_case", "input", "wind", "prepro",
                                 area, "translation.txt")
   
+  dp_file <- file.path(path, "test_case", "input", "wind", "prepro",
+                       area, "k.txt")
+  
+  conv_file <- file.path(path, "test_case", "input", "wind", "prepro",
+                         area, "conversion.txt")
+  
   expect_equal(
-    list(fread(coefficients_file), fread(translation_file)),
-    list(as.data.table(M), as.data.table(V1))
+    list(fread(coefficients_file), fread(translation_file), fread(dp_file), fread(conv_file)),
+    list(as.data.table(M), as.data.table(V1), as.data.table(dp), as.data.table(cv))
+  )
+  
+  expect_error(
+    writeSeriesPrepro(area = area, type = "wind",
+                      coefficients = M,
+                      overwrite = FALSE),
+    regexp = "already exist"
   )
 })
 
