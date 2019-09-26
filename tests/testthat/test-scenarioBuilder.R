ff81bb87361d466007535922b57146d2e5269aee
Etienne <etienne.sanchez@dreamrs.fr>
2019-09-26 15:42:59 +0200

    Added tests for readScenarioBuilder() and updateScenarioBuilder()

--- tests/testthat/test-scenarioBuilder.R
+++ tests/testthat/test-scenarioBuilder.R
@@ -67,7 +67,63 @@ test_that("Warning is thrown when n_mc differs from nbyears", {
   
 })
 
+test_that("readScenarioBuilder works", {
+  
+  expect_identical(
+    readScenarioBuilder(),
+    list(
+      l = structure(
+        list(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 
+             2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L), .Dim = c(9L, 2L), .Dimnames = list(
+               c("a", "a_offshore", "b", "c", "hub", "psp in", "psp in-2", 
+                 "psp out", "psp out-2"), NULL)),
+      t = structure(list(1L, 2L), .Dim = 1:2, .Dimnames = list("a", NULL))
+    )
+  )
+  
+  expect_warning(readScenarioBuilder(ruleset = "fake ruleset name"))
+  
+})
 
+test_that("updateScenarioBuilder works", {
+  
+  m <- scenarioBuilder(
+    n_scenario = 2,
+    n_mc = 2,
+    areas = c("fr", "it", "be"),
+    areas_rand = c("it", "be")
+  )
+  
+  m2 <- scenarioBuilder(
+    n_scenario = 2,
+    n_mc = 2,
+    areas = c("ie", "it", "be"),
+    areas_rand = "be"
+  )
+  
+  expect_error(updateScenarioBuilder(ldata = m))
+  
+  expect_error(updateScenarioBuilder(ldata = m, series = "h"), NA)
+  
+  updateScenarioBuilder(ldata = list(w = m, s = m2))
+  
+  newSB <- readScenarioBuilder(as_matrix = TRUE)
+  
+  m <- m[m[, 1] != "rand", , drop = FALSE]
+  m_out <- apply(m, 2, as.integer)
+  m_out <- as.list(m_out) # to match actual output of readScenarioBuilder()
+  attributes(m_out) <- attributes(m)
+  
+  expect_identical(newSB[["w"]], m_out)
+  
+  m2 <- m2[m2[, 1] != "rand", , drop = FALSE]
+  m2_out <- apply(m2, 2, as.integer)
+  m2_out <- as.list(m2_out)
+  attributes(m2_out) <- attributes(m2)
+
+  expect_identical(newSB[["s"]], m2_out)
+  
+})
 
 # End ---------------------------------------------------------------------
 
