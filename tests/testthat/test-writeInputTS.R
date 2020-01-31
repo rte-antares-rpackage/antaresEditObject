c917d2e744181e05c62bfb7ffde5cb0c6824008d
Etienne <etienne.sanchez@dreamrs.fr>
2020-01-31 16:00:45 +0100

    Fixed error in writeInputTs()

--- tests/testthat/test-writeInputTS.R
+++ tests/testthat/test-writeInputTS.R
@@ -14,25 +14,25 @@ sapply(studies, function(study) {
     
     M <- matrix(c(rep(8, 8760), rep(5.1, 8760)), nrow = 8760)
     
-    writeInputTS(area = area, type = "thermal", data = M)
+    writeInputTS(area = area, type = "solar", data = M)
     
-    values_file <- file.path(path, "test_case", "input", "thermal", "series",
-                             paste0("thermal_", area, ".txt"))
+    values_file <- file.path(path, "test_case", "input", "solar", "series",
+                             paste0("solar_", area, ".txt"))
     
     expect_equal(fread(values_file), as.data.table(M))
     
     expect_error(
-      writeInputTS(area = "fake area", type = "thermal", data = M),
+      writeInputTS(area = "fake area", type = "solar", data = M),
       regexp = "not a valid area"
     )
     
     expect_error(
-      writeInputTS(area = area, type = "thermal", data = M, overwrite = FALSE),
+      writeInputTS(area = area, type = "solar", data = M, overwrite = FALSE),
       regexp = "already exist"
     )
     
     expect_error(
-      writeInputTS(area = area, type = "thermal", data = matrix(1:3)),
+      writeInputTS(area = area, type = "solar", data = matrix(1:3)),
       regexp = "8760\\*N matrix"
     )
     
