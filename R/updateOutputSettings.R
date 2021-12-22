ca7d5d0dee4f17f8fcc71a96644fd57a851a1964
pvictor <victor.perrier@dreamRs.fr>
2021-12-22 14:43:37 +0100

    updateInputSettings & updateOutputSettings: API OK

--- R/updateOutputSettings.R
+++ R/updateOutputSettings.R
@@ -1,15 +1,19 @@
+#' @title Update output parameters of an Antares study
+#' 
+#' @description 
+#' `r antaresEditObject::badge_api_ok()`
+#' 
 #' Update output parameters of an Antares study
+#' 
 #'
 #' @param synthesis Logical. If TRUE, synthetic results will be stored in a
 #'   directory Study_name/OUTPUT/simu_tag/Economy/mc-all. If FALSE, No general
 #'   synthesis will be printed out.
 #' @param storenewset Logical. See Antares General Reference Guide.
 #' @param archives Character vector. Series to archive.
-#' @param opts
-#'   List of simulation parameters returned by the function
-#'   \code{antaresRead::setSimulationPath}
-#'
-#' @return An updated list containing various information about the simulation.
+#' 
+#' @template opts
+#' 
 #' @export
 #'
 #' @importFrom assertthat assert_that
@@ -17,19 +21,40 @@
 #' @examples
 #' \dontrun{
 #'
-#' updateOutputSettings(synthesis = TRUE, storenewset = FALSE,
+#' updateOutputSettings(synthesis = TRUE,
+#'                      storenewset = FALSE,
 #'                      archives = c("load", "wind"))
 #' 
 #' }
 #'
-updateOutputSettings <- function(
-  synthesis = NULL,
-  storenewset = NULL,
-  archives = NULL,
-  opts = antaresRead::simOptions()
-  ) {
+updateOutputSettings <- function(synthesis = NULL,
+                                 storenewset = NULL,
+                                 archives = NULL,
+                                 opts = antaresRead::simOptions()) {
   
   assertthat::assert_that(inherits(opts, "simOptions"))
+  
+  # API block
+  if (is_api_study(opts)) {
+    
+    cmd <- api_command_generate(
+      action = "update_config",
+      target = "settings/generaldata/output",
+      data = list(
+        synthesis = synthesis,
+        storenewset = storenewset,
+        archives = paste(archives, collapse = ", ")
+      )
+    )
+    api_command_register(cmd, opts = opts)
+    `if`(
+      should_command_be_executed(opts), 
+      api_command_execute(cmd, opts = opts, text_alert = "Updating output settings: {msg_api}"),
+      cli_command_registered("update_config")
+    )
+    
+    return(invisible(opts))
+  }
   
   pathIni <- file.path(opts$studyPath, "settings", "generaldata.ini")
   general <- readIniFile(file = pathIni)
