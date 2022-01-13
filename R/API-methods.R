d1cdd55cf4e24c4524e38ab2e1725a64128c439c
pvictor <victor.perrier@dreamRs.fr>
2022-01-13 12:29:33 +0100

    use token if present

--- R/API-methods.R
+++ R/API-methods.R
@@ -1,41 +1,65 @@
 
-#' @importFrom httr GET accept_json stop_for_status content
+#' @importFrom httr GET accept_json stop_for_status content add_headers
 api_get <- function(opts, url, ...) {
+  config <- list(
+    accept_json()
+  )
+  if (!is.null(opts$token) && opts$token != "") {
+    config <- c(config, list(
+      add_headers(Authorization = paste("Bearer", opts$token))
+    ))
+  }
   result <- GET(
     url = sprintf(
       "%s/v1/studies/%s",
       opts$host, url
     ),
-    accept_json(),
+    config = config,
     ...
   )
   stop_for_status(result)
   content(result)
 }
 
-#' @importFrom httr POST accept_json content_type_json stop_for_status content 
+#' @importFrom httr POST accept_json content_type_json stop_for_status content add_headers
 api_post <- function(opts, url, ...) {
+  config <- list(
+    accept_json(),
+    content_type_json()
+  )
+  if (!is.null(opts$token) && opts$token != "") {
+    config <- c(config, list(
+      add_headers(Authorization = paste("Bearer", opts$token))
+    ))
+  }
   result <- POST(
     url = sprintf(
       "%s/v1/studies/%s",
       opts$host, url
     ),
-    accept_json(),
-    content_type_json(),
+    config = config,
     ...
   )
   stop_for_status(result)
   content(result)
 }
 
-#' @importFrom httr PUT accept_json stop_for_status content 
+#' @importFrom httr PUT accept_json stop_for_status content add_headers
 api_put <- function(opts, url, ...) {
+  config <- list(
+    accept_json()
+  )
+  if (!is.null(opts$token) && opts$token != "") {
+    config <- c(config, list(
+      add_headers(Authorization = paste("Bearer", opts$token))
+    ))
+  }
   result <- PUT(
     url = sprintf(
       "%s/v1/studies/%s",
       opts$host, url
     ),
-    accept_json(),
+    config = config,
     ...
   )
   stop_for_status(result)
@@ -44,12 +68,20 @@ api_put <- function(opts, url, ...) {
 
 #' @importFrom httr DELETE accept_json stop_for_status content 
 api_delete <- function(opts, url, ...) {
+  config <- list(
+    accept_json()
+  )
+  if (!is.null(opts$token) && opts$token != "") {
+    config <- c(config, list(
+      add_headers(Authorization = paste("Bearer", opts$token))
+    ))
+  }
   result <- DELETE(
     url = sprintf(
       "%s/v1/studies/%s",
       opts$host, url
     ),
-    accept_json(),
+    config = config,
     ...
   )
   stop_for_status(result)
