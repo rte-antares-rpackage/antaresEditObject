c89eef5e198bf50fcbfe66336d648f4eef1042ba
MANSOURI Assil Ext <assil.mansouri@rte-france.com>
2023-03-17 09:35:20 +0100

    devs backup zip

--- R/backupStudy.R
+++ R/backupStudy.R
@@ -3,7 +3,7 @@
 #' @description 
 #' `r antaresEditObject:::badge_api_no()`
 #' 
-#' Save an Antares Study or only inputs in a \code{.tar.gz} file
+#' Save an Antares Study or only inputs in a \code{.tar.gz} or \code{.zip} file
 #'
 #' @param backupfile Name of the backup, without extension. If missing, 
 #' either the name of the study or 'input' according argument \code{what}.
@@ -12,6 +12,7 @@
 #' @param opts
 #'   List of simulation parameters returned by the function
 #'   \code{antaresRead::setSimulationPath}
+#' @param extension Defaut is {.zip}.
 #'
 #' @return The path of the backup
 #' @export
@@ -23,16 +24,18 @@
 #' \dontrun{
 #' backupStudy()
 #' }
-backupStudy <- function(backupfile, what = c("input", "study"), opts = antaresRead::simOptions()) {
+backupStudy <- function(backupfile, what = c("input", "study"), 
+                        opts = antaresRead::simOptions(), extension = ".zip") {
   
   what <- match.arg(arg = what)
   
   assertthat::assert_that(inherits(opts, "simOptions"))
+  assertthat::assert_that(extension %in% c(".tar.gz", ".zip"))
   api_not_implemented(opts)
   assertthat::assert_that(!is.null(opts$studyPath) && dir.exists(opts$studyPath))
   
   if (missing(backupfile))
-    backupfile <- ifelse(what == "study", opts$studyName, what)
+    backupfile <- ifelse(what == "study", opts$studyPath, what)
   
   # backupfile <- file.path(dirname(opts$studyPath), paste0(backupfile, ".tar.gz"))
   
@@ -47,19 +50,17 @@ backupStudy <- function(backupfile, what = c("input", "study"), opts = antaresRe
   setwd(zip_dir)
   
   tryCatch(
-    utils::tar(
-      tarfile = paste0(backupfile, ".tar.gz"),
-      files = zip_files,
-      compression = "gzip"
-    )
+    if (extension == ".tar.gz")
+      utils::tar(tarfile = paste0(backupfile, ".tar.gz"), files = zip_files, compression = "gzip")
+    else utils::zip(zipfile = paste0(backupfile, ".zip"), files = zip_files)
     , error = function(e) {
-      stop("Could not write ", shQuote(paste0(backupfile, ".tar.gz")), " [", e$message, "]")
+      stop("Could not write ", shQuote(paste0(backupfile, extension)), " [", e$message, "]")
     }
     , finally = {
       setwd(curr_wd)
   })
   
-  res <- file.path(zip_dir, paste0(backupfile, ".tar.gz"))
+  res <- file.path(zip_dir, paste0(backupfile, extension))
   
   if( !file.exists(res) ){
     stop("Failed to create backup", call. = FALSE)
