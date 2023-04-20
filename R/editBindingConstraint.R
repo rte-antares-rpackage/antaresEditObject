67038f1dbe5b5ca9e9903c6730afd88bb03afbd5
BERTHET Clement Ext <clement.berthet@rte-france.com>
2023-04-20 15:13:50 +0200

    EditBindingConstraint v860 maj doc

--- R/editBindingConstraint.R
+++ R/editBindingConstraint.R
@@ -6,7 +6,7 @@
 #' Update an existing binding constraint in an Antares study.
 #' 
 #'
-#' @inheritParams create-binding-constraint
+#' @inheritParams createBindingConstraint
 #' @template opts
 #' 
 #' @seealso [createBindingConstraint()] to create new binding constraints, [removeBindingConstraint()] to remove binding constraints.
