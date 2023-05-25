0e947d84631be88bb0a5d23745d890fffa437571
BERTHET Clement Ext <clement.berthet@rte-france.com>
2023-05-25 15:41:15 +0200

    create/edit st maj test with readInputTS

--- tests/testthat/test-createClusterST.R
+++ tests/testthat/test-createClusterST.R
@@ -30,15 +30,16 @@ test_that("Create short-term storage cluster (new feature v8.6)",{
                                            opts = opts_test))
       
     # check cluster exists with default values
-    createClusterST(area_test, 
+    opts_test <- createClusterST(area_test, 
                     "cluster1", 
                     opts = opts_test) 
+    
+    opts_test$antaresVersion <- 860
     
     testthat::expect_true(paste(area_test, "cluster1", sep = "_") 
                 %in% levels(readClusterSTDesc(opts = opts_test)$cluster))
     
-    # read series
-    # TODO => test with readInputTS
+    # read series (with fread_antares)
     file_series <- antaresRead:::fread_antares(opts = opts_test, 
                                 file = file.path(path_master, 
                                                  "series",
@@ -48,6 +49,39 @@ test_that("Create short-term storage cluster (new feature v8.6)",{
     # check default value and dimension
     testthat::expect_equal(dim(file_series), c(8760, 1))
     testthat::expect_equal(mean(file_series$V1), 0)
+    
+    # read series (with readInputTS)
+    st_ts <- readInputTS(st_storage = "all", opts = opts_test)
+    
+      # check to find 5 names files created previously
+    files_names <- unique(st_ts$name_file)
+    
+    testthat::expect_equal(c("inflows", "lower-rule-curve", "PMAX-injection", "PMAX-withdrawal" , "upper-rule-curve"),
+                           files_names)
+    
+      # chech default values of txt files
+    storage_value <- list(PMAX_injection = list(N=1, string = "PMAX-injection"),
+                          PMAX_withdrawal = list(N=1, string = "PMAX-withdrawal"),
+                          inflows = list(N=0, string = "inflows"),
+                          lower_rule_curve = list(N=0, string = "lower-rule-curve"),
+                          upper_rule_curve = list(N=1, string = "upper-rule-curve"))
+    
+    real_names_cols <- unlist(lapply(storage_value, `[[`, 2), use.names = FALSE)
+    names(storage_value) <- real_names_cols
+    
+    df_ref_default_value <- data.table::setDT(lapply(storage_value, `[[`, 1), )
+    df_ref_default_value <- melt(df_ref_default_value, 
+                                 variable.name = "name_file", 
+                                 value.name = "mean", 
+                                 variable.factor = FALSE)
+    
+    df_ref_default_value <- df_ref_default_value[base::order(df_ref_default_value$name_file)]
+    
+      # mean of default TS created
+    test_txt_value <- st_ts[, list(mean=mean(`st-storage`)), by=name_file]
+    
+    # check default values
+    testthat::expect_equal(df_ref_default_value$mean, test_txt_value$mean)
     
     # createClusterST throws error when cluster already exist.
     testthat::expect_error(createClusterST(area_test, 
--- tests/testthat/test-editClusterST.R
+++ tests/testthat/test-editClusterST.R
@@ -19,19 +19,23 @@ test_that("edit st-storage clusters (only for study >= v8.6.0" , {
   writeIni(NULL, file.path(path_master, "clusters", area_test, "list"))
   
   # create tests clusters
-  createClusterST(area_test, 
+  opts_test <- createClusterST(area_test, 
                   "cluster-st-1", 
                   opts = opts_test) 
   
-  createClusterST(area_test, 
+  opts_test$antaresVersion <- 860
+  
+  opts_test <- createClusterST(area_test, 
                   "cluster-st-2", 
                   opts = opts_test) 
+  
+  opts_test$antaresVersion <- 860
   
   st_clusters <- readClusterSTDesc(opts = opts_test)
   
   # edit cluster
   val <- 0.007
-  editClusterST(area = area_test, 
+  opts_test <- editClusterST(area = area_test, 
                 cluster_name = levels(st_clusters$cluster)[1], 
                 test_param1= "test",
                 test_param2= 0.002154,
@@ -43,6 +47,8 @@ test_that("edit st-storage clusters (only for study >= v8.6.0" , {
                 opts = opts_test, 
                 add_prefix = FALSE)
   
+  opts_test$antaresVersion <- 860
+  
   st_clusters <- readClusterSTDesc(opts = opts_test)
   
   res <- st_clusters[cluster == levels(st_clusters$cluster)[1], test.param1, test.param2]
@@ -50,8 +56,7 @@ test_that("edit st-storage clusters (only for study >= v8.6.0" , {
   # test parameters values edited
   testthat::expect_true(all(res %in% c(0.002154, "test")))
   
-  # test data value 
-  # TODO => test with readInputTS
+  # test data value (with fread_antares)
   path_dir_test <- file.path(opts_test$inputPath, "st-storage", "series", area_test, 
                              paste(area_test, "cluster-st-1", sep = "_"))
   files_test <- list.files(path_dir_test, full.names = TRUE)
@@ -62,6 +67,12 @@ test_that("edit st-storage clusters (only for study >= v8.6.0" , {
                          mean(.x$V1)}))
   
   testthat::expect_equal(value_test, val)
+  
+  # test data value with readInputTS
+  st_ts <- readInputTS(st_storage = "all", opts = opts_test)
+  
+  # check clusters
+  testthat::expect_true(all(levels(st_clusters$cluster) %in% unique(st_ts$cluster)))
   
   # delete study
   unlink(opts_test$studyPath, recursive = TRUE)
