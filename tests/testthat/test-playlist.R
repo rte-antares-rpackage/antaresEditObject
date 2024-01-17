4784f75ba5414e407edc40f7af6e13703e591149
kemihak <kamel.kemiha@rte-france.com>
2024-01-17 14:40:26 +0100

    Add unit test for setPlaylist()

--- tests/testthat/test-playlist.R
+++ tests/testthat/test-playlist.R
@@ -28,3 +28,35 @@ sapply(studies, function(study) {
   unlink(x = file.path(pathstd, "test_case"), recursive = TRUE)
   
 })
+
+
+test_that("Check if setPlaylist() is disabled when playlist = mcYear and enabled when playlist is customized", {
+  
+  ant_version <- "8.2.0"
+  st_test <- paste0("my_study_820_", paste0(sample(letters,5),collapse = ""))
+  suppressWarnings(opts <- createStudy(path = pathstd, study_name = st_test, antares_version = ant_version))
+  
+  # Default behaviour
+  expect_false(opts$parameters$general$`user-playlist`)
+  
+  # Set parameters
+  updateGeneralSettings(nbyears = 10)
+  suppressWarnings(opts <- setSimulationPath(opts$studyPath, simulation = "input"))
+  nbyears <- opts$parameters$general$nbyears
+  mc_years <- seq(nbyears)
+  
+  # With custom playlist - user-playlist is enabled
+  playlist_to_write <- sort(sample(mc_years, size = nbyears/2, replace = FALSE))
+  setPlaylist(playlist = playlist_to_write, opts = opts)
+  suppressWarnings(opts <- setSimulationPath(opts$studyPath, simulation = "input"))
+  playlist_in_file <- getPlaylist(opts = opts)
+  
+  expect_true(opts$parameters$general$`user-playlist`)
+  expect_equal(playlist_to_write, playlist_in_file)
+  
+  # No playlist if length(playlist) == length(mcYear) - user-playlist is disabled
+  setPlaylist(playlist = mc_years, opts = opts)
+  suppressWarnings(opts <- setSimulationPath(opts$studyPath, simulation = "input"))
+  
+  expect_false(opts$parameters$general$`user-playlist`)
+})
