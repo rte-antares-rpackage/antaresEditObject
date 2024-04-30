a1c0953bfa54c73a270cebc064ea796570d7d140
berthetclement <clement.berthet013@gmail.com>
2024-04-30 15:34:22 +0200

    createBindingConstraint() update doc

--- R/createBindingConstraint.R
+++ R/createBindingConstraint.R
@@ -19,7 +19,8 @@
 #' @param filter_year_by_year Marginal price granularity for year by year
 #' @param filter_synthesis Marginal price granularity for synthesis
 #' @param coefficients A named list containing the coefficients used by the constraint, 
-#' the coefficients have to be alphabetically ordered.
+#' the coefficients have to be alphabetically ordered see examples below for entering 
+#' weight or weight with offset.
 #' @param group "character" group of the constraint, default value : "default group"
 #' @param overwrite If the constraint already exist, overwrite the previous value.
 #' 
