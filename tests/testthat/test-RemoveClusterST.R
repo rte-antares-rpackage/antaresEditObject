a2a3e4d1f012a3f0f92e915dbd3cf72e9ea2cfec
berthetclement <clement.berthet013@gmail.com>
2025-03-25 16:10:44 +0100

    add tests for removeClusterST() from 8.6 to 9.2

--- tests/testthat/test-RemoveClusterST.R
+++ tests/testthat/test-RemoveClusterST.R
@@ -1,10 +1,152 @@
 # >=860 ----
 
-testthat::test_that("remove st storage", {
-  # # RemoveClusterST (if no cluster => function read return error => see readClusterDesc tests)
-  # opts_test <- removeClusterST(area = area_test_clust, "cluster1", 
-  #                              opts = opts_test)
-  # 
-  # testthat::expect_false(paste(area_test_clust, "cluster1", sep = "_") %in% 
-  #                          levels(readClusterSTDesc(opts = opts_test)$cluster))
+test_that("New feature v8.6", {
+  suppressWarnings(
+    createStudy(path = tempdir(), 
+                study_name = "st-storage", 
+                antares_version = "8.6.0"))
+  
+  # just need at least one area
+  area_test_clust = "al" 
+  createArea(name = area_test_clust)
+  
+  testthat::test_that("remove the only one cluster", {
+    # at least need 1 st cluster
+    createClusterST(area = area_test_clust, 
+                    cluster_name = "cluster_init")
+    
+    # remove cluster
+    removeClusterST(area = area_test_clust, 
+                    cluster_name = "cluster_init")
+    
+    # removed from ini file ?
+    
+    # read prop
+    path_st_ini <- file.path("input", 
+                             "st-storage", 
+                             "clusters", 
+                             area_test_clust,
+                             "list")
+    
+    read_ini <- antaresRead::readIni(path_st_ini)
+    target_prop <- read_ini[[paste(area_test_clust, 
+                                   "cluster_init",
+                                   sep = "_")]]
+    
+    testthat::expect_null(target_prop)
+    
+    # remove directory ? 
+    path_st_dir <- file.path("input", 
+                             "st-storage", 
+                             "clusters", 
+                             "series",
+                             area_test_clust,
+                             "al_cluster_init")
+    
+    testthat::expect_false(dir.exists(path_st_dir))
+  })
+  
+  deleteStudy()
+})
+
+# >=880 ----
+
+testthat::test_that("New feature v8.8", {
+  suppressWarnings(
+    createStudy(path = tempdir(), 
+                study_name = "st-storage", 
+                antares_version = "8.8.0"))
+  
+  # just need at least one area
+  area_test_clust = "al" 
+  createArea(name = area_test_clust)
+  
+  testthat::test_that("remove the only one cluster", {
+    # at least need 1 st cluster
+    createClusterST(area = area_test_clust, 
+                    cluster_name = "cluster_init")
+    
+    # remove cluster
+    removeClusterST(area = area_test_clust, 
+                    cluster_name = "cluster_init")
+    
+    # removed from ini file ?
+    
+    # read prop
+    path_st_ini <- file.path("input", 
+                             "st-storage", 
+                             "clusters", 
+                             area_test_clust,
+                             "list")
+    
+    read_ini <- antaresRead::readIni(path_st_ini)
+    target_prop <- read_ini[[paste(area_test_clust, 
+                                   "cluster_init",
+                                   sep = "_")]]
+    
+    testthat::expect_null(target_prop)
+    
+    # remove directory ? 
+    path_st_dir <- file.path("input", 
+                             "st-storage", 
+                             "clusters", 
+                             "series",
+                             area_test_clust,
+                             "al_cluster_init")
+    
+    testthat::expect_false(dir.exists(path_st_dir))
+  })
+  
+  deleteStudy()
+})
+
+# >=920 ----
+
+testthat::test_that("New feature v9.2", {
+  suppressWarnings(
+    createStudy(path = tempdir(), 
+                study_name = "st-storage", 
+                antares_version = "9.2"))
+  
+  # just need at least one area
+  area_test_clust = "al" 
+  createArea(name = area_test_clust)
+  
+  testthat::test_that("remove the only one cluster", {
+    # at least need 1 st cluster
+    createClusterST(area = area_test_clust, 
+                    cluster_name = "cluster_init")
+    
+    # remove cluster
+    removeClusterST(area = area_test_clust, 
+                    cluster_name = "cluster_init")
+    
+    # removed from ini file ?
+    
+    # read prop
+    path_st_ini <- file.path("input", 
+                             "st-storage", 
+                             "clusters", 
+                             area_test_clust,
+                             "list")
+    
+    read_ini <- antaresRead::readIni(path_st_ini)
+    target_prop <- read_ini[[paste(area_test_clust, 
+                                   "cluster_init",
+                                   sep = "_")]]
+    
+    testthat::expect_null(target_prop)
+    
+    # remove directory ? 
+    path_st_dir <- file.path("input", 
+                             "st-storage", 
+                             "clusters", 
+                             "series",
+                             area_test_clust,
+                             "al_cluster_init")
+    
+    testthat::expect_false(dir.exists(path_st_dir))
+  })
+  
+  deleteStudy()
 })
