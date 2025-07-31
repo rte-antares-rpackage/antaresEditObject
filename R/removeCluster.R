337ca0b22782635608fdedddbcc9abdb57e72228
Clément Berthet <clement.berthet013@gmail.com>
2025-07-31 15:44:16 +0200

    removeClusterST() manage constraints (#224)
    * removeClusterST() manage constraints
    
    * create/edit minor fix for prefix and directory name
    
    * removeClusterST() updated to remove constraints properties and TS files

--- R/removeCluster.R
+++ R/removeCluster.R
@@ -81,7 +81,6 @@ removeClusterST <- function(area,
                            add_prefix = TRUE,
                            cluster_type = c("thermal", "renewables", "st-storage"),
                            opts = antaresRead::simOptions()) {
-  
   cluster_type <- match.arg(cluster_type)
   
   # tolower area ----
@@ -169,9 +168,18 @@ removeClusterST <- function(area,
   }
   
   dirs_to_remove <- file.path(clustertypePath, subdirs_to_remove, area)
+  
+  # remove dir "constraints" (st-storage)
+  if(opts$antaresVersion>=920 & cluster_type %in% "st-storage")
+    dirs_to_remove <- file.path(clustertypePath, 
+                                append(subdirs_to_remove, "constraints"), 
+                                area)
+  
+  # if list.ini is empty, area/series area/constraints can be removed
   if (length(previous_params) > 0) {
     dirs_to_remove <- file.path(dirs_to_remove, cluster_name)
   }
+  
   lapply(dirs_to_remove, unlink, recursive = TRUE)
   
   # Maj simulation
