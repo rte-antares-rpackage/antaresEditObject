8b20a409fa848f37d7e1ea6458a053bcaa500ca9
kemihak <kamel.kemiha@rte-france.com>
2025-10-22 22:06:07 +0200

    Add unit tests for createDistrict()

--- tests/testthat/test-district.R
+++ tests/testthat/test-district.R
@@ -13,6 +13,57 @@ sapply(studies, function(study) {
     
     areas <- sort(sample(x = getOption("antares")$areaList, size = 2))
     
+    expect_error(  
+      createDistrict(
+        name = "MyDistrict", 
+        apply_filter = "remove-all", 
+        remove_area = areas,
+        add_area = areas,
+        opts = opts
+      )
+      , regexp = "You can not use 'add_area' and 'remove_area' at the same time"
+    )
+    
+    expect_error(  
+      createDistrict(
+        name = "MyDistrict", 
+        apply_filter = "remove-all", 
+        remove_area = c(areas, "fake_area"),
+        opts = opts
+      )
+      , regexp = "Invalid area in 'remove_area'"
+    )
+
+    expect_error(  
+      createDistrict(
+        name = "MyDistrict", 
+        apply_filter = "remove-all", 
+        add_area = c(areas, "fake_area"),
+        opts = opts
+      )
+      , regexp = "Invalid area in 'add_area'"
+    )
+    
+    expect_error(  
+      createDistrict(
+        name = "MyDistrict", 
+        apply_filter = "remove-all", 
+        remove_area = areas,
+        opts = opts
+      )
+      , regexp = "You have to use 'remove_area' with 'apply_filter' set to add-all"
+    )
+
+    expect_error(  
+      createDistrict(
+        name = "MyDistrict", 
+        apply_filter = "add-all", 
+        add_area = areas,
+        opts = opts
+      )
+      , regexp = "You have to use 'add_area' with 'apply_filter' set to remove-all"
+    )
+    
     createDistrict(
       name = "MyDistrict", 
       apply_filter = "add-all", 
@@ -23,10 +74,6 @@ sapply(studies, function(study) {
   })
   
   test_that("Remove a district", {
-    
-    expect_warning(removeDistrict(name = "mydistrict", opts = simOptions()),
-                   regexp = "No district was removed. Please provide the exact name of the district."
-                   )
     
     removeDistrict(
       name = "MyDistrict", 
