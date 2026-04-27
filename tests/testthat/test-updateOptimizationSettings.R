f0b6e3115df64a1c562d738367288a55c69035a3
kemihak <kamel.kemiha@rte-france.com>
2026-04-27 14:42:28 +0200

    Update tests

--- tests/testthat/test-updateOptimizationSettings.R
+++ tests/testthat/test-updateOptimizationSettings.R
@@ -31,7 +31,7 @@ sapply(studies, function(study) {
 })
 
 
-test_that("solver.log parameter available only if version >= 8.8", {
+test_that("solver.log parameter available only if version >= 8.8, update multiple properties and check log message if value is not authorized", {
   
   ant_version <- "8.2.0"
   st_test <- paste0("my_study_820_", paste0(sample(letters,5),collapse = ""))
@@ -52,6 +52,10 @@ test_that("solver.log parameter available only if version >= 8.8", {
               )
   updateOptimizationSettings(include.unfeasible.problem.behavior = "error-dry", opts = opts)
   expect_equal(getOption("antares")$parameters$optimization$`include-unfeasible-problem-behavior`, "error-dry")
+  
+  updateOptimizationSettings(simplex.range = "day", include.tc.min.stable.power = "true", opts = opts)
+  expect_equal(getOption("antares")$parameters$optimization$`simplex-range`, "day")
+  expect_true(getOption("antares")$parameters$optimization$`include-tc-minstablepower`)
   
   unlink(x = opts$studyPath, recursive = TRUE)
 })
